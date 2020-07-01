6cccd61fbe7617869ce9a427c2f6cb19fd8a518a
teranich <teran4work@gmail.com>
2020-07-01 13:21:00 +0300

    Updates

---
+++ precache-manifest.779da8f6f08623c8507c003c9fe781f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1465392d2dd65fb5ab0cccd48d202850",
+    "url": "/careful-reader/index.html"
+  },
+  {
+    "revision": "876a8c87d288480c35c3",
+    "url": "/careful-reader/static/css/main.d1f5ea2a.chunk.css"
+  },
+  {
+    "revision": "9c8e86cda3d7121b6d28",
+    "url": "/careful-reader/static/js/2.13120a8e.chunk.js"
+  },
+  {
+    "revision": "876a8c87d288480c35c3",
+    "url": "/careful-reader/static/js/main.3f7b4d18.chunk.js"
+  },
+  {
+    "revision": "18c9985920a30c2097fc",
+    "url": "/careful-reader/static/js/runtime-main.f97e0d03.js"
+  }
+]);
