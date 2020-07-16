4b67072acc644a46f83edf2d77f9cdeae14dbb5d
Toly <teran4work@gmail.com>
2020-07-16 13:29:31 +0300

    Updates

---
+++ precache-manifest.5f4bb6abbc71bc321a8777aa072422ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c0283280a16dacf7bf355823c7dab3",
+    "url": "/careful-reader/index.html"
+  },
+  {
+    "revision": "8b3c60ae4adb9cc70825",
+    "url": "/careful-reader/static/css/main.45ef97e5.chunk.css"
+  },
+  {
+    "revision": "7b7080cd17403dc6a091",
+    "url": "/careful-reader/static/js/2.b2e5706f.chunk.js"
+  },
+  {
+    "revision": "8b3c60ae4adb9cc70825",
+    "url": "/careful-reader/static/js/main.f7e6f456.chunk.js"
+  },
+  {
+    "revision": "18c9985920a30c2097fc",
+    "url": "/careful-reader/static/js/runtime-main.f97e0d03.js"
+  }
+]);
