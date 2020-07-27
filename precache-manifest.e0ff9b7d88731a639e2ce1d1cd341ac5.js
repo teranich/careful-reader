c2f6a7fff2a9febb38fd39b8b6a99e28480c9708
Toly <teran4work@gmail.com>
2020-07-27 21:44:22 +0300

    Updates

--- precache-manifest.e0ff9b7d88731a639e2ce1d1cd341ac5.js
+++ precache-manifest.e0ff9b7d88731a639e2ce1d1cd341ac5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d2697fcf9364cf51d5a9653aa36be8c",
+    "revision": "70d990923fcaa077b60d447bdada6070",
     "url": "/careful-reader/index.html"
   },
   {
-    "revision": "b90c359c5f29f87e1f3e",
+    "revision": "140f1f006069e43d9867",
     "url": "/careful-reader/static/css/main.45ef97e5.chunk.css"
   },
   {
-    "revision": "7b7080cd17403dc6a091",
-    "url": "/careful-reader/static/js/2.b2e5706f.chunk.js"
+    "revision": "2bb178fb799b614d728a",
+    "url": "/careful-reader/static/js/2.7a3af07a.chunk.js"
   },
   {
-    "revision": "b90c359c5f29f87e1f3e",
-    "url": "/careful-reader/static/js/main.b38ef4c1.chunk.js"
+    "revision": "140f1f006069e43d9867",
+    "url": "/careful-reader/static/js/main.4024109a.chunk.js"
   },
   {
     "revision": "18c9985920a30c2097fc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/careful-reader/precache-manifest.ceb6de5da5ce8ecfd6955ac38e9e6e44.js"
+  "/careful-reader/precache-manifest.e0ff9b7d88731a639e2ce1d1cd341ac5.js"
 );
 
 self.addEventListener('message', (event) => {
