5b0e607e40c2230d5c6ad9c2ce4d838857bdc7ed
Toly <teran4work@gmail.com>
2020-08-03 13:32:25 +0300

    Add hook for google drive api

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,47 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import './Header.scss'
-
-function useGoogleAuth() {
-  const [loaded, setLoaded] = useState(false)
-  const [googleAuth, setGoogleAuth] = useState<gapi.auth2.GoogleAuth>()
-
-  useEffect(() => {
-    window.gapi?.load('auth2', () => {
-      window.gapi?.auth2
-        .init({
-          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-          scope: 'https://www.googleapis.com/auth/drive.appdata',
-        })
-        .then(oauth2ok, oauth2Error)
-    })
-
-    function oauth2ok(auth2: gapi.auth2.GoogleAuth) {
-      if (auth2) {
-        setGoogleAuth(auth2)
-        setLoaded(true)
-      }
-    }
-
-    function oauth2Error() {}
-  }, [])
-
-  const signIn = () => googleAuth?.signIn()
-  const signOut = () => googleAuth?.signOut()
-
-  return { signIn, signOut, loaded }
-}
-
-function useGoogleDrive() {}
+import useGoogleDrive from '../../uitls/auth'
 
 const Header: React.FC = ({ children }) => {
-  const { signIn, signOut, loaded } = useGoogleAuth()
-
+  const { isClientLoaded, isLoggedIn, signIn, signOut } = useGoogleDrive()
   return (
     <header className="header">
       {children}
-      <div className={loaded ? '' : 'hidden'}>
-        <button onClick={signIn}>LogIn</button>
-        <button onClick={signOut}>LogOut</button>
+      <div className={isClientLoaded ? '' : 'hidden'}>
+        {isLoggedIn ? (
+          <button onClick={signOut}>Log Out</button>
+        ) : (
+          <button onClick={signIn}>Log In</button>
+        )}
       </div>
     </header>
   )
