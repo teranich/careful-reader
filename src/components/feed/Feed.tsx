011823253a2ae2bb452fbf1017d24494bf291f2f
teranich <teran4work@gmail.com>
2020-09-02 23:59:54 +0300

    Add link for current book to feed

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import LibraryStoreContext from '../../store/LibraryStore'
+import { observer } from 'mobx-react'
+import { Book } from '../../types'
+import BookItem from '../common/BookItem'
+import BookShelfImage from './book-shelf.png'
+import './feed.scss'
+import { Header } from '../common'
 
-export default function Feed() {
-  return (
-    <>
-      <ul>
-        <li>
-          <Link to="/shelves">Shelves</Link>
-        </li>
-      </ul>
-    </>
+export default observer(function Feed() {
+  const { currentBook } = useContext(
+    LibraryStoreContext
   )
-}
+  const { meta }: { meta: Book } = currentBook
+
+  return (
+    <div className="feed">
+      <Header></Header>
+      <div className="content">
+        {meta.id &&
+          <section className="current-book">
+            <span className="title">to read...</span>
+            <BookItem book={meta} to={`/read/${meta.id}`} />
+          </section>}
+        <section className="link-to-shelves">
+          <span className="title">Library</span>
+          <Link to="/shelves" >
+            <img src={BookShelfImage} alt="shelve" />
+          </Link>
+        </section>
+      </div>
+    </div>
+  )
+})
