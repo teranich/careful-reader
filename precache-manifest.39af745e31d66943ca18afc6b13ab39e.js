9e595b733e0f4ba80e4fd3ef151d2f3bc3818031
teranich <teran4work@gmail.com>
2020-09-03 23:34:19 +0300

    Updates

---
+++ precache-manifest.39af745e31d66943ca18afc6b13ab39e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7288dd181aca73f9d186962b665482dd",
+    "url": "/careful-reader/index.html"
+  },
+  {
+    "revision": "f77232b15abcbe0550dc",
+    "url": "/careful-reader/static/css/main.091d8c04.chunk.css"
+  },
+  {
+    "revision": "68301c80c3636207fb21",
+    "url": "/careful-reader/static/js/2.6633a7a6.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/careful-reader/static/js/2.6633a7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77232b15abcbe0550dc",
+    "url": "/careful-reader/static/js/main.080dacad.chunk.js"
+  },
+  {
+    "revision": "686cabb9b1f7cd6e4a7c",
+    "url": "/careful-reader/static/js/runtime-main.c16cb572.js"
+  },
+  {
+    "revision": "60dcdcf864d173a786c5d050109cb80a",
+    "url": "/careful-reader/static/media/book-shelf.60dcdcf8.png"
+  },
+  {
+    "revision": "3e06a93b70c6188db772ebd7408970dc",
+    "url": "/careful-reader/static/media/home.3e06a93b.svg"
+  },
+  {
+    "revision": "6a85ca81ee211ce6e79b993d592995ca",
+    "url": "/careful-reader/static/media/page2.6a85ca81.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/careful-reader/precache-manifest.b912da6d1c8fbd612647f6ebd615c2b1.js"
+  "/careful-reader/precache-manifest.39af745e31d66943ca18afc6b13ab39e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/careful-reader/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
