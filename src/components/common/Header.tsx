6e7057eea418254a07fd31d23bfbf6abba48f18a
teranich <teran4work@gmail.com>
2020-10-03 18:35:26 +0300

    Use react material design

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,21 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.scss'
 import { observer } from 'mobx-react'
-import { BackButton, HomeButton, AuthButtons, SettingsButton } from '../controls'
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+import TuneIcon from '@material-ui/icons/Tune';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import { AuthButtons } from '../controls';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  menuButton: {
+    marginRight: '',
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
 
 export default observer(function Header({ children, className = '' }: any) {
+  const classes = useStyles();
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDrawer = (state: boolean) => (event: any) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setIsOpen(state)
+  };
+
+  const list = () => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <Link to="/">
+          <ListItem button>
+            <ListItemIcon><DashboardIcon /></ListItemIcon>
+            <ListItemText primary="Feed" />
+          </ListItem>
+        </Link>
+        <Link to="/shelves">
+          <ListItem button>
+            <ListItemIcon><LibraryBooksIcon /></ListItemIcon>
+            <ListItemText primary="Library" />
+          </ListItem>
+        </Link>
+        <Link to="/settings">
+          <ListItem button>
+            <ListItemIcon><TuneIcon /></ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+    </div>
+  );
   return (
-    <header className={`header ${className}`}>
-      <div className="left-content">
-        <BackButton />
-        <HomeButton />
-        <SettingsButton />
-      </div>
-      <div className="children">{children} </div>
-      <div className="right-content">
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <React.Fragment>
+            <MenuIcon onClick={toggleDrawer(true)} />
+            <Drawer anchor="left" open={isOpen} onClose={toggleDrawer(false)}>
+              {list()}
+            </Drawer>
+          </React.Fragment>
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+      </Typography>
         <AuthButtons />
-      </div>
-    </header>
+      </Toolbar>
+    </AppBar>
   )
 })
