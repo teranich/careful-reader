03790c01ad97e1be06b4bd4fec3539674bef439d
teranich <teran4work@gmail.com>
2020-10-20 14:52:08 +0300

    Use fab btn for add book

--- src/pages/shelves/Shelves.tsx
+++ src/pages/shelves/Shelves.tsx
@@ -1,78 +1,55 @@
-import React, { ReactNode, useContext } from 'react'
-import './Shelves.scss'
-
+import React from 'react'
 import { Header } from '../../components/common'
 import { observer } from 'mobx-react'
 import RemoteBooksList from './RemoteShelve'
 import { AddBookButton } from './common'
 import LocalBooksList from './LocalShelve'
-import { Loading } from '../../components/loading'
-import LibraryStoreContext from '../../store/LibraryStore'
-import Container from '@material-ui/core/Container';
 import SwipeableViews from 'react-swipeable-views';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-
-function TabPanel(props: any) {
-  const { children, value, index } = props;
-  return (
-    <>
-      {value === index && (
-        <Container >
-          {children}
-        </Container>
-      )}
-    </>
-  );
-}
+import Zoom from '@material-ui/core/Zoom';
+import AddIcon from '@material-ui/icons/Add';
+import './Shelves.scss'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    width: 500,
+  fab: {
+    position: 'absolute', bottom: theme.spacing(10),
+    right: theme.spacing(2),
+  },
+  bottomMenu: {
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    left: 0
   }
 }));
-
-const bottomMenu: React.CSSProperties = {
-  position: 'fixed',
-  bottom: 0,
-  right: 0,
-  left: 0
-}
-function ElevationScroll({ children }: any) {
-
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 0,
-    target: window
-  });
-
-  return React.cloneElement(children, {
-    elevation: trigger ? 4 : 0,
-  });
-}
 
 export default observer(function Shelves() {
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
-
+  const transitionDuration = {
+    enter: theme.transitions.duration.enteringScreen,
+    exit: theme.transitions.duration.leavingScreen,
+  };
   const handleChangeIndex = (index: number) => (setValue(index))
   const handleChange = (event: any, index: number) => (handleChangeIndex(index))
+  const classes = useStyles()
+
+  const fabs = [
+    {
+      color: 'primary',
+      icon: <AddIcon />,
+      label: 'Add',
+      clickHandler: () => {
+        console.log('hey you')
+      }
+    }
+  ];
 
   return (
     <>
       <Header>
-
       </Header>
       <SwipeableViews index={value} onChangeIndex={handleChangeIndex} className="swipeableViews">
         <LocalBooksList />
@@ -81,8 +58,20 @@ export default observer(function Shelves() {
           favorite
         </div>
       </SwipeableViews>
-
-      <div style={bottomMenu}>
+      {fabs.map((fab, index) => (
+        <Zoom
+          key={fab.color}
+          in={value === index}
+          timeout={transitionDuration}
+          style={{
+            transitionDelay: `${value === index ? transitionDuration.exit : 0}ms`,
+          }}
+          unmountOnExit
+        >
+          <AddBookButton />
+        </Zoom>
+      ))}
+      <div className={classes.bottomMenu}>
         <BottomNavigation
           value={value}
           onChange={handleChange}
@@ -92,9 +81,7 @@ export default observer(function Shelves() {
           <BottomNavigationAction label="remote" />
           <BottomNavigationAction label="fav" />
         </BottomNavigation>
-
       </div>
-      {/* <AddBookButton className="add-button-fixed" /> */}
     </>
   )
 })
--- src/pages/shelves/common.tsx
+++ src/pages/shelves/common.tsx
@@ -1,12 +1,30 @@
 import { observer } from 'mobx-react'
 import { readFileContent } from '../../uitls/common'
 import React, { useContext } from 'react'
-
-import AddBookIcon from './add-book.png'
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import LibraryStoreContext from '../../store/LibraryStore'
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    position: 'absolute',
+    right: 0,
+    bottom: theme.spacing(2),
+  },
+  fab: {
+    right: theme.spacing(2),
+  },
+  input: {
+    display: 'none'
+  }
+}));
 
 const AddBookButton = observer(({ ...rest }: any) => {
   const { addBookAction } = useContext(LibraryStoreContext)
+  const theme = useTheme();
+  const classes = useStyles(theme)
+
   function onChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
     if (event.target.files?.length) {
       const file = event.target.files[0]
@@ -17,18 +35,23 @@ const AddBookButton = observer(({ ...rest }: any) => {
   }
 
   return (
-    <div {...rest}>
-      <label className="add-book-lable" htmlFor="btn-add-book">
-        <img src={AddBookIcon} alt="" />
-        <input
-          type="file"
-          name=""
-          id="btn-add-book"
-          onChange={onChangeHandler}
-          accept=".fb2"
-        />
-      </label>
-    </div>
+    <label className={classes.container} {...rest} htmlFor="btn-add-book">
+      <input
+        type="file"
+        name=""
+        id="btn-add-book"
+        onChange={onChangeHandler}
+        accept=".fb2"
+        className={classes.input}
+      />
+      <Fab
+        color="primary"
+        component="span"
+        className={classes.fab}>
+        <AddIcon />
+      </Fab>
+
+    </label>
   )
 })
 
