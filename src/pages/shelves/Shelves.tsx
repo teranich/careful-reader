d8da7e9acf437acd45a0442b8ed9519ef4d989e8
teranich <teran4work@gmail.com>
2020-12-21 21:54:48 +0300

    Fix scrolling in shelve page

--- src/pages/shelves/Shelves.tsx
+++ src/pages/shelves/Shelves.tsx
@@ -4,36 +4,42 @@ import { observer } from 'mobx-react'
 import RemoteBooksList from './RemoteShelve'
 import { AddBookButton } from './common'
 import LocalBooksList from './LocalShelve'
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import Zoom from '@material-ui/core/Zoom';
-import AddIcon from '@material-ui/icons/Add';
+import SwipeableViews from 'react-swipeable-views'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import Zoom from '@material-ui/core/Zoom'
+import AddIcon from '@material-ui/icons/Add'
 import './Shelves.scss'
+import styled from 'styled-components'
 
+const Content = styled.div`
+  height: 100vh;
+  overflow: auto;
+`
 const useStyles = makeStyles((theme) => ({
   fab: {
-    position: 'absolute', bottom: theme.spacing(10),
+    position: 'absolute',
+    bottom: theme.spacing(10),
     right: theme.spacing(2),
   },
   bottomMenu: {
     position: 'fixed',
     bottom: 0,
     right: 0,
-    left: 0
-  }
-}));
+    left: 0,
+  },
+}))
 
 export default observer(function Shelves() {
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+  const theme = useTheme()
+  const [value, setValue] = React.useState(0)
   const transitionDuration = {
     enter: theme.transitions.duration.enteringScreen,
     exit: theme.transitions.duration.leavingScreen,
-  };
-  const handleChangeIndex = (index: number) => (setValue(index))
-  const handleChange = (event: any, index: number) => (handleChangeIndex(index))
+  }
+  const handleChangeIndex = (index: number) => setValue(index)
+  const handleChange = (event: any, index: number) => handleChangeIndex(index)
   const classes = useStyles()
 
   const fabs = [
@@ -43,40 +49,38 @@ export default observer(function Shelves() {
       label: 'Add',
       clickHandler: () => {
         console.log('hey you')
-      }
-    }
-  ];
+      },
+    },
+  ]
 
   return (
     <>
-      <Header>
-      </Header>
-      <SwipeableViews index={value} onChangeIndex={handleChangeIndex} className="swipeableViews">
-        <LocalBooksList />
-        <RemoteBooksList />
-        <div>
-          favorite
-        </div>
-      </SwipeableViews>
-      {fabs.map((fab, index) => (
-        <Zoom
-          key={fab.color}
-          in={value === index}
-          timeout={transitionDuration}
-          style={{
-            transitionDelay: `${value === index ? transitionDuration.exit : 0}ms`,
-          }}
-          unmountOnExit
-        >
-          <AddBookButton />
-        </Zoom>
-      ))}
+      <Header></Header>
+      <Content>
+        <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
+          <LocalBooksList />
+          <RemoteBooksList />
+          <div>favorite</div>
+        </SwipeableViews>
+        {fabs.map((fab, index) => (
+          <Zoom
+            key={fab.color}
+            in={value === index}
+            timeout={transitionDuration}
+            style={{
+              transitionDelay: `${
+                value === index ? transitionDuration.exit : 0
+              }ms`,
+            }}
+            unmountOnExit
+          >
+            <AddBookButton />
+          </Zoom>
+        ))}
+      </Content>
+
       <div className={classes.bottomMenu}>
-        <BottomNavigation
-          value={value}
-          onChange={handleChange}
-          showLabels
-        >
+        <BottomNavigation value={value} onChange={handleChange} showLabels>
           <BottomNavigationAction label="local" />
           <BottomNavigationAction label="remote" />
           <BottomNavigationAction label="fav" />
