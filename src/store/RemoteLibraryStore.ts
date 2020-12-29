766368537d6cade64a3f92f5a7ce431d3d433e57
teranich <teran4work@gmail.com>
2020-12-29 15:27:07 +0300

    Refactor cloud usage

--- src/store/RemoteLibraryStore.ts
+++ src/store/RemoteLibraryStore.ts
@@ -1,72 +1,64 @@
 import React from 'react'
-import GAPI from '../uitls/googleAPI'
 import { observable, action } from 'mobx'
 import { Book, RemoteBook } from '../types'
+import * as cloud from '../uitls/cloud'
 
 export const RemoteLibraryStore = () => {
-  const gapi = new GAPI()
+  const initLibrary = action(async () => {
+    cloud.drive.create.folder('books-folder')
+  })
+  cloud.load().then(() => {
+    store.isClientLoaded = true
+    cloud.isLoggedIn().then((isLoggedIn) => (store.isLoggedIn = isLoggedIn))
+  })
 
   const fetchBooksListAction = action(async () => {
     store.isBooksLoading = true
-    store.books = await gapi.list(`fileExtension="json"`)
+    // store.books = await gapi.list(`fileExtension="json"`)
     store.isBooksLoading = false
   })
 
-  const signInAction = action(async () => {
-    await gapi.signIn()
-    store.isLoggedIn = gapi.isLoggedIn()
-  })
-
-  const signOutAction = action(async () => {
-    await gapi.signOut()
-    store.isLoggedIn = gapi.isLoggedIn()
-    store.books = []
-  })
-
   const uploadBookAction = action(async (book: Book, bookText: string) => {
-    const list = await gapi.list(`name="${book.name}-text.html"`)
-    if (list.length) return
-
-    const [metaFile, textFile] = await Promise.all([
-      gapi.createFile(`${book.name}-meta.json`),
-      gapi.createFile(`${book.name}-text.html`),
-    ])
-
-    book.metaFileId = metaFile.id
-    book.textFileId = textFile.id
-    await Promise.all([
-      gapi.upload(metaFile.id, JSON.stringify(book)),
-      gapi.upload(textFile.id, bookText),
-    ])
-
-    const newBook: RemoteBook = {
-      id: metaFile.id,
-      metaFileId: metaFile.id,
-      textFileId: textFile.id,
-      name: `${book.name}-meta.json`,
-    }
-    store.books.push(newBook)
+    // const list = await gapi.list(`name="${book.name}-text.html"`)
+    // if (list.length) return
+    // const [metaFile, textFile] = await Promise.all([
+    //   gapi.createFile(`${book.name}-meta.json`),
+    //   gapi.createFile(`${book.name}-text.html`),
+    // ])
+    // book.metaFileId = metaFile.id
+    // book.textFileId = textFile.id
+    // await Promise.all([
+    //   gapi.upload(metaFile.id, JSON.stringify(book)),
+    //   gapi.upload(textFile.id, bookText),
+    // ])
+    // const newBook: RemoteBook = {
+    //   id: metaFile.id,
+    //   metaFileId: metaFile.id,
+    //   textFileId: textFile.id,
+    //   name: `${book.name}-meta.json`,
+    // }
+    // store.books.push(newBook)
   })
 
   const syncMetaAction = action(async (book: Book) => {
-    if (book.metaFileId) {
-      await gapi.upload(book.metaFileId, JSON.stringify(book))
-    }
+    // if (book.metaFileId) {
+    //   await gapi.upload(book.metaFileId, JSON.stringify(book))
+    // }
   })
 
   const downloadBookAction = action(async (book: RemoteBook) => {
-    const meta: any = await gapi.download(book.id)
-    let text: any = ''
-    if (meta.textFileId) text = await gapi.download(meta.textFileId)
-    return { meta, text }
+    // const meta: any = await gapi.download(book.id)
+    // let text: any = ''
+    // if (meta.textFileId) text = await gapi.download(meta.textFileId)
+    // return { meta, text }
   })
 
   const removeBookAction = action(async (book: any) => {
-    const isSuccess = await gapi.deleteFile(book.id)
-    if (isSuccess) {
-      store.books = store.books.filter((inner) => inner.id !== book.id)
-    }
-    return isSuccess
+    // const isSuccess = await gapi.deleteFile(book.id)
+    // if (isSuccess) {
+    //   store.books = store.books.filter((inner) => inner.id !== book.id)
+    // }
+    // return isSuccess
   })
 
   const store = observable({
@@ -74,8 +66,7 @@ export const RemoteLibraryStore = () => {
     isClientLoaded: false,
     isLoggedIn: false,
     isBooksLoading: false,
-    signInAction,
-    signOutAction,
+    initLibrary,
     uploadBookAction,
     syncMetaAction,
     downloadBookAction,
@@ -83,15 +74,6 @@ export const RemoteLibraryStore = () => {
     fetchBooksListAction,
   })
 
-  gapi
-    .load()
-    .then(() => {
-      store.isClientLoaded = true
-      store.isLoggedIn = gapi.isLoggedIn()
-    })
-    .catch((err) => {
-      console.error('error is', err)
-    })
   return store
 }
 export default React.createContext<any>({})
---
+++ src/uitls/cloud.ts
@@ -0,0 +1,127 @@
+import { importScript } from './common'
+
+interface Files {
+  id: string
+}
+
+let loaded = false
+async function loadGapi() {
+  if (loaded) return true
+  return importScript('https://apis.google.com/js/client:platform.js')
+    .then(() => {
+      return new Promise((resolve, reject) =>
+        window.gapi.load('auth2', () => resolve(window.gapi.auth2))
+      )
+    })
+    .then(() => {
+      loaded = true
+    })
+    .catch(() => {
+      console.error('google api is not loaded')
+    })
+}
+
+let isGAPILoaded = false
+const googleDriveAPIDocument =
+  'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
+const scope =
+  'https://www.googleapis.com/auth/drive.appfolder https://www.googleapis.com/auth/drive.file'
+export async function load() {
+  if (isGAPILoaded) return
+  await loadGapi()
+  return new Promise((resolve, reject) => {
+    return window.gapi.client
+      .init({
+        apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+        clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        discoveryDocs: [googleDriveAPIDocument],
+        scope: scope,
+      })
+      .then(() => (isGAPILoaded = true))
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
+export async function signIn() {
+  await load()
+  try {
+    return window.gapi.auth2.getAuthInstance().signIn()
+  } catch (e) {
+    console.error('error in signIn', e)
+  }
+}
+
+export async function signOut() {
+  await load()
+  try {
+    return window.gapi.auth2.getAuthInstance().signOut()
+  } catch (e) {
+    console.error('error in signOut', e)
+  }
+}
+
+export async function isLoggedIn() {
+  await load()
+  try {
+    return (
+      window.gapi.auth2.getAuthInstance() &&
+      window.gapi.auth2.getAuthInstance().isSignedIn.get()
+    )
+  } catch (e) {
+    console.error('error in isLoggedIn', e)
+    return false
+  }
+}
+const folderMIME = 'application/vnd.google-apps.folder'
+const fileMIME = 'text/plain'
+
+const create = (parents: string[]) => (type: string) => async (
+  name: string
+) => {
+  const mimeType = type === 'folder' ? folderMIME : fileMIME
+  console.log('create', parents, type, name)
+  const resp = await promisify(gapi.client.drive.files.create, {
+    resource: {
+      name: name,
+      mimeType,
+      parents: ['appDataFolder'],
+    },
+    fields: 'id',
+  })
+  return resp.result
+}
+
+export const drive = {
+  create: {
+    folder: create(['drive'])('folder'),
+    file: create(['drive'])('file'),
+  },
+}
+
+function promisify(
+  gapiFn: any,
+  options: any
+): Promise<gapi.client.Response<Files>> {
+  return new Promise((resolve, reject) => {
+    return gapiFn(options).then(
+      (resp: any) => {
+        if (resp && (resp.status < 200 || resp.status > 299)) {
+          console.error('GAPI call returned bad status', resp)
+          reject(resp)
+        } else {
+          resolve(resp)
+        }
+      },
+      (err: any) => {
+        return reject(err)
+      }
+    )
+  })
+}
+
+export let x = 0
+
+export const inc = () => {
+  return x++
+}
